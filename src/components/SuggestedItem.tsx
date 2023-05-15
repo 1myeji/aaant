9b509ea8af4bfb97e82f9f0810e35a58b6b8946c
imyeji <yimyg0596@naver.com>
2023-05-15 18:03:44 +0900

    Feat: 긴 추천 검색어를 잘라서 말줄임표 추가하기

--- src/components/SuggestedItem.tsx
+++ src/components/SuggestedItem.tsx
@@ -2,9 +2,11 @@ import React from 'react';
 import { type ISuggestedItemProps } from '../types/global';
 
 function SuggestedItem({ suggestedItem }: ISuggestedItemProps) {
+  if (typeof suggestedItem !== 'string') throw new Error('suggestedItem must be a string');
+  const Item = suggestedItem?.length > 47 ? `${suggestedItem.slice(0, 47)}...` : suggestedItem;
   return (
     <li className="item">
-      <span>{suggestedItem}</span>
+      <span>{Item}</span>
     </li>
   );
 }
